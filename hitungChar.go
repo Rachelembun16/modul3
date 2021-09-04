eb021d397238d2e4681009c538cc82b21e02ef74
Rachel <rachelembunsafira@gmail.com>
2021-09-04 21:08:57 +0700

    tambah hitung karakter sama

--- hitungChar.go
+++ hitungChar.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	var input string
 
 	fmt.Println("===HITUNG KARAKTER===")
-	fmt.Print("Masukkan suatu kata=")
+	fmt.Println("")
+	fmt.Print("Masukkan suatu kata= ")
 	fmt.Scanln(&input)
-
 	hasil := len(input)
-
 	fmt.Printf("jumlah karakter = %d", hasil)
 	fmt.Println("")
 	fmt.Printf("Karakter yang diinputkan = %s", input)
+	fmt.Println("")
+
+	for i := 0; i < hasil-1; i++ {
+		for j := i + 1; j < len(input); j++ {
+			if string(input[i]) == string(input[j]) {
+				var jumlah = strings.Count(input, string(input[i]))
+				fmt.Printf("Jumlah %s = %d", string(input[i]), jumlah)
+				fmt.Println("")
+			}
+		}
+	}
+
 }
